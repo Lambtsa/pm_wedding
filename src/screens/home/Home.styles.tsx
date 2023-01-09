ddd2f1c47ab055e57ce8315bc83fff9c2b736d93
Tom Lamb <lambtsa.tom@gmail.com>
2023-01-09 22:53:00 +0100

    Update with slanted design

---
+++ src/screens/home/Home.styles.tsx
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+
+export const FirstDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: #bb3e17;
+  width: 100%;
+  height: 100vh;
+  color: #fffeeb;
+`;
+
+export const SecondDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: #bb3e17;
+  width: 100%;
+  height: 200px;
+  color: #fffeeb;
+  clip-path: polygon(0 0, 100% 0, 100% 30%, 0 100%);
+`;
+
+export const ThirdDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: #e8ae68;
+  width: 100%;
+  height: 200px;
+  margin-top: -140px;
+  color: #fffeeb;
+  clip-path: polygon(0 70%, 100% 0, 100% 100%, 0 100%);
+`;
+
+export const ForthDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: #e8ae68;
+  width: 100%;
+  height: 100vh;
+  color: #fffeeb;
+`;
+
+export const FifthDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: #e8ae68;
+  width: 100%;
+  height: 200px;
+  color: #fffeeb;
+  clip-path: polygon(0 0, 100% 0, 100% 30%, 0 100%);
+`;
+
+export const SixthDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: #bb3e17;
+  width: 100%;
+  height: 200px;
+  margin-top: -140px;
+  color: #fffeeb;
+  clip-path: polygon(0 70%, 100% 0, 100% 100%, 0 100%);
+`;
+
+export const SeventhDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: #9a3212;
+  width: 100%;
+  height: 200px;
+  margin-top: -140px;
+  color: #fffeeb;
+  clip-path: polygon(0 70%, 100% 0, 100% 100%, 0 100%);
+`;
+
+export const EighthDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  background-color: #9a3212;
+  width: 100%;
+  height: 100vh;
+  color: #fffeeb;
+`;
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,3 +1,27 @@
+import {
+  EighthDiv,
+  FifthDiv,
+  FirstDiv,
+  ForthDiv,
+  SecondDiv,
+  SeventhDiv,
+  SixthDiv,
+  ThirdDiv,
+} from "./Home.styles";
+
 export const HomeScreen = (): JSX.Element => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <FirstDiv>Hello World</FirstDiv>
+      <SecondDiv />
+      <ThirdDiv />
+      <ForthDiv>Hello World</ForthDiv>
+      <FifthDiv />
+      <SixthDiv />
+      <FirstDiv>Hello World</FirstDiv>
+      <SecondDiv />
+      <SeventhDiv />
+      <EighthDiv>Hello World</EighthDiv>
+    </>
+  );
 };
